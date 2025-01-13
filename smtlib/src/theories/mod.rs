b6a468d33d670c5925af98ddd1289060d15163f4
Sankalp Gambhir <sankalp.gambhir42@gmail.com>
2025-01-13 15:20:41 +0100

    Export ArraysEx

--- smtlib/src/theories/mod.rs
+++ smtlib/src/theories/mod.rs
@@ -1,6 +1,7 @@
 //! Theories in SMT-LIB are definitions of [sorts](crate::Sort) and in general
 //! functions present in the [logics](crate::Logic).
 
+pub mod arrays_ex;
 pub mod core;
 pub mod fixed_size_bit_vectors;
 pub mod ints;
