5892a754a93609d96b99643c8dd2e872bb4a70f3
Jared Carlson <jared.carlson23@gmail.com>
2025-06-01 23:33:26 -0400

    a bit of cleanup.

--- smtlib/examples/socrates.rs
+++ smtlib/examples/socrates.rs
@@ -1,7 +1,5 @@
 use miette::IntoDiagnostic;
-use smtlib::{backend::z3_binary::Z3Binary, funs::Fun, sorts::Sort, Bool, Storage};
-use smtlib_lowlevel::{ast, lexicon::{Symbol, Numeral}};
-use smtlib::terms::STerm;
+use smtlib::{backend::z3_binary::Z3Binary, funs::Fun, sorts::Sort, Bool, Storage, terms};
 
 fn main() -> miette::Result<()> {
     // Set up miette error handling
@@ -13,15 +11,12 @@ fn main() -> miette::Result<()> {
     // Create a new solver using Z3
     let mut solver = smtlib::Solver::new(&st, Z3Binary::new("z3").into_diagnostic()?)?;
 
-    // Set the logic to UF (Uninterpreted Functions) using a custom logic string
+    // Set the logic to UF (Uninterpreted Functions)
     solver.set_logic(smtlib::Logic::Custom("UF".to_string()))?;
 
     // Declare a new sort 'S' for entities (humans, etc.)
     let s_sort = Sort::new(&st, "S");
     
-    // Declare the sort to the solver using run_command
-    solver.run_command(ast::Command::DeclareSort(Symbol("S"), Numeral::from_usize(0)))?;
-
     // Create functions with proper sort parameters
     let human = Fun::new(&st, "Human", vec![s_sort.clone()], Bool::sort());
     let mortal = Fun::new(&st, "Mortal", vec![s_sort.clone()], Bool::sort());
@@ -32,9 +27,8 @@ fn main() -> miette::Result<()> {
 
     // Declare a constant 'Socrates' of sort 'S'
     let socrates = s_sort.new_const(&st, "Socrates");
-    solver.run_command(ast::Command::DeclareConst(Symbol("Socrates"), s_sort.ast()))?;
-
-    // Create a variable 'x' for universal quantification
+    
+    // Create a variable 'x' of sort 'S' for quantification
     let x = s_sort.new_const(&st, "x");
     
     // Build the formula: ForAll([x], Implies(Human(x), Mortal(x)))
@@ -43,14 +37,16 @@ fn main() -> miette::Result<()> {
     let human_implies_mortal = human_x.as_bool()?.implies(mortal_x.as_bool()?);
     
     // Create a SortedVar for the quantifier
+    // We need to use the SortedVar from lowlevel for the quantifier
+    // This is the only part where we need to use the lowlevel API
     let x_var = smtlib_lowlevel::ast::SortedVar(
         smtlib_lowlevel::lexicon::Symbol("x"),
         s_sort.ast()
     );
     let sorted_vars = st.alloc_slice(&[x_var]);
-    let all_humans_mortal = STerm::new(
+    let all_humans_mortal = smtlib::terms::STerm::new(
         &st,
-        smtlib_lowlevel::ast::Term::Forall(sorted_vars, STerm::from(human_implies_mortal).into())
+        smtlib_lowlevel::ast::Term::Forall(sorted_vars, smtlib::terms::STerm::from(human_implies_mortal).into())
     ).into();
     solver.assert(all_humans_mortal)?;
 
@@ -60,8 +56,7 @@ fn main() -> miette::Result<()> {
 
     // Build and assert: Not(Mortal(Socrates))
     let mortal_socrates = mortal.call(&[socrates.into()])?;
-    // solver.assert(!mortal_socrates.as_bool()?)?;
-    solver.assert(mortal_socrates.as_bool()?)?;
+    solver.assert(!mortal_socrates.as_bool()?)?;
 
     // Check satisfiability
     let result = solver.check_sat()?;
@@ -75,18 +70,17 @@ fn main() -> miette::Result<()> {
     println!("(declare-const Socrates S)");
     println!("(assert (forall ((x S)) (=> (Human x) (Mortal x))))");
     println!("(assert (Human Socrates))");
-    println!("(assert (Mortal Socrates))");
+    println!("(assert (not (Mortal Socrates)))");
     println!("(check-sat)");
     
     println!("\nResult: {}", result);
     println!("\nExplanation:");
-    println!("- We have the premise \"All humans are mortal\" represented as a universal quantification");
-    println!("- We have the premise \"Socrates is human\"");
-    println!("- We assert the negation of the conclusion \"Socrates is mortal\"");
-    println!("- The solver returns \"unsat\" (unsatisfiable), which means there is no model");
-    println!("  where all the assertions are true simultaneously");
-    println!("- This proves that the negation of our conclusion leads to a contradiction");
-    println!("- Therefore, the conclusion \"Socrates is mortal\" must be true");
+    println!("The result should be 'unsat' (unsatisfiable), which proves the syllogism:");
+    println!("1. All humans are mortal (forall x, Human(x) => Mortal(x))");
+    println!("2. Socrates is human (Human(Socrates))");
+    println!("3. Therefore, Socrates is mortal (Mortal(Socrates))");
+    println!("By asserting the negation of the conclusion (not Mortal(Socrates))");
+    println!("and getting 'unsat', we've proven that the conclusion must be true.");
 
     Ok(())
 }
